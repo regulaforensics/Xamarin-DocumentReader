4a0fd9c93e0049314a2ebffbb42ce1be381e2ed9
Jenkins <jenkins@regula.by>
2023-04-10 12:08:35 +0300

    auto update

--- DocumentReaderSample/Scenario.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace DocumentReaderSample
-{
-    public class Scenario
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
