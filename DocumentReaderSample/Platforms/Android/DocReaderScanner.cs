2b24a59f801f0980a66cc026657d80158ea55d81
Pavel Masiuk <masukpasha@gmail.com>
2024-07-02 15:21:49 +0300

    Revert "test"
    This reverts commit 1b3bb7cf82f8ef6177b876a0b90cc60c3b7dfcd1.

--- DocumentReaderSample/Platforms/Android/DocReaderScanner.cs
+++ DocumentReaderSample/Platforms/Android/DocReaderScanner.cs
@@ -1,11 +1,11 @@
 ﻿using Android.Graphics;
-// using Com.Regula.Documentreader.Api;
-// using Com.Regula.Documentreader.Api.Config;
-// using Com.Regula.Documentreader.Api.Completions;
-// using Com.Regula.Documentreader.Api.Completions.Rfid;
-// using Com.Regula.Documentreader.Api.Enums;
-// using Com.Regula.Documentreader.Api.Errors;
-// using Com.Regula.Documentreader.Api.Results;
+using Com.Regula.Documentreader.Api;
+using Com.Regula.Documentreader.Api.Config;
+using Com.Regula.Documentreader.Api.Completions;
+using Com.Regula.Documentreader.Api.Completions.Rfid;
+using Com.Regula.Documentreader.Api.Enums;
+using Com.Regula.Documentreader.Api.Errors;
+using Com.Regula.Documentreader.Api.Results;
 
 namespace DocumentReaderSample.Platforms.Android
 {
@@ -15,33 +15,33 @@ namespace DocumentReaderSample.Platforms.Android
         public byte[] PortraitField { get; set; }
         public byte[] DocumentField { get; set; }
     }
-    public class DocReaderScanner : Java.Lang.Object, IDocReaderScanner
+    public class DocReaderScanner : Java.Lang.Object, IDocReaderScanner, IDocumentReaderCompletion
     {
         public event EventHandler<IDocReaderScannerEvent> ResultsObtained;
         private bool IsReadRfid = false;
         private string selectedScenario = "Mrz";
         public void ShowScanner(bool IsReadRfid)
         {
-            // ScannerConfig config = new ScannerConfig.Builder(selectedScenario).Build();
-            // DocumentReader.Instance().ShowScanner(Platform.AppContext, config, this);
-            // this.IsReadRfid = IsReadRfid;
+            ScannerConfig config = new ScannerConfig.Builder(selectedScenario).Build();
+            DocumentReader.Instance().ShowScanner(Platform.AppContext, config, this);
+            this.IsReadRfid = IsReadRfid;
         }
-        // public void OnCompleted(int action, DocumentReaderResults results, DocumentReaderException error)
-        // {
-        //     if (action != DocReaderAction.Complete && action != DocReaderAction.Timeout) return;
-        //     if (IsReadRfid && results != null && results.ChipPage != 0)
-        //     {
-        //         DocumentReader.Instance().StartRFIDReader(Platform.AppContext, new RfidCallback(this));
-        //         IsReadRfid = false;
-        //         return;
-        //     }
-        //     ResultsObtained(this, new DocReaderScannerEvent
-        //     {
-        //         SurnameAndGivenNames = results.GetTextFieldValueByType(EVisualFieldType.FtSurnameAndGivenNames),
-        //         PortraitField = ConvertBitmap(results.GetGraphicFieldImageByType(EGraphicFieldType.GfPortrait)),
-        //         DocumentField = ConvertBitmap(results.GetGraphicFieldImageByType(EGraphicFieldType.GfDocumentImage))
-        //     });
-        // }
+        public void OnCompleted(int action, DocumentReaderResults results, DocumentReaderException error)
+        {
+            if (action != DocReaderAction.Complete && action != DocReaderAction.Timeout) return;
+            if (IsReadRfid && results != null && results.ChipPage != 0)
+            {
+                DocumentReader.Instance().StartRFIDReader(Platform.AppContext, new RfidCallback(this));
+                IsReadRfid = false;
+                return;
+            }
+            ResultsObtained(this, new DocReaderScannerEvent
+            {
+                SurnameAndGivenNames = results.GetTextFieldValueByType(EVisualFieldType.FtSurnameAndGivenNames),
+                PortraitField = ConvertBitmap(results.GetGraphicFieldImageByType(EGraphicFieldType.GfPortrait)),
+                DocumentField = ConvertBitmap(results.GetGraphicFieldImageByType(EGraphicFieldType.GfDocumentImage))
+            });
+        }
         public void SelectScenario(string scenarioName) { selectedScenario = scenarioName; }
         private static Bitmap CompressBitmap(Bitmap bitmap)
         {
@@ -64,17 +64,17 @@ namespace DocumentReaderSample.Platforms.Android
         public void RecognizeImage(Stream stream, bool IsReadRfid)
         {
             Bitmap bitmap = BitmapFactory.DecodeStream(stream);
-            // RecognizeConfig config = new RecognizeConfig.Builder(selectedScenario).SetBitmap(bitmap).Build();
-            // DocumentReader.Instance().Recognize(Platform.AppContext, config, this);
+            RecognizeConfig config = new RecognizeConfig.Builder(selectedScenario).SetBitmap(bitmap).Build();
+            DocumentReader.Instance().Recognize(Platform.AppContext, config, this);
             this.IsReadRfid = IsReadRfid;
         }
     }
-    // public class RfidCallback(DocReaderScanner scanner) : IRfidReaderCompletion
-    // {
-    //     public DocReaderScanner instance = scanner;
-    //     public override void OnCompleted(int action, DocumentReaderResults results, DocumentReaderException error)
-    //     {
-    //         instance.OnCompleted(action, results, error);
-    //     }
-    // }
+    public class RfidCallback(DocReaderScanner scanner) : IRfidReaderCompletion
+    {
+        public DocReaderScanner instance = scanner;
+        public override void OnCompleted(int action, DocumentReaderResults results, DocumentReaderException error)
+        {
+            instance.OnCompleted(action, results, error);
+        }
+    }
 }
