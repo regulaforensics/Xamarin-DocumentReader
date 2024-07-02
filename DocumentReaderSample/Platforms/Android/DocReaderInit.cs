1b3bb7cf82f8ef6177b876a0b90cc60c3b7dfcd1
Pavel Masiuk <masukpasha@gmail.com>
2024-07-02 14:26:14 +0300

    test

--- DocumentReaderSample/Platforms/Android/DocReaderInit.cs
+++ DocumentReaderSample/Platforms/Android/DocReaderInit.cs
@@ -1,14 +1,14 @@
 ﻿using Android.Bluetooth;
 using Android.Content;
 using Android.OS;
-using AndroidX.Core.App;
-using Com.Regula.Documentreader.Api;
-using Com.Regula.Documentreader.Api.Ble;
-using Com.Regula.Documentreader.Api.Completions;
-using Com.Regula.Documentreader.Api.Errors;
-using Com.Regula.Documentreader.Api.Internal.Permission;
-using Com.Regula.Documentreader.Api.Params;
-using Com.Regula.Documentreader.Api.Results;
+// using AndroidX.Core.App;
+// using Com.Regula.Documentreader.Api;
+// using Com.Regula.Documentreader.Api.Ble;
+// using Com.Regula.Documentreader.Api.Completions;
+// using Com.Regula.Documentreader.Api.Errors;
+// using Com.Regula.Documentreader.Api.Internal.Permission;
+// using Com.Regula.Documentreader.Api.Params;
+// using Com.Regula.Documentreader.Api.Results;
 using Android.App;
 using Android.Content.PM;
 using Android;
@@ -22,7 +22,7 @@ namespace DocumentReaderSample.Platforms.Android
         public bool IsSuccess { get; set; }
         public bool IsRfidAvailable { get; set; }
     }
-    public class DocReaderInit : BleWrapperCallback, IDocReaderInit, IDocumentReaderInitCompletion, IServiceConnection
+    public class DocReaderInit : IDocReaderInit
     {
         public event EventHandler<IDocReaderInitEvent> ScenariosObtained;
         public void InitDocReader()
@@ -34,67 +34,67 @@ namespace DocumentReaderSample.Platforms.Android
                 streamReader.BaseStream.CopyTo(memstream);
                 bytes = memstream.ToArray();
             }
-            DocReaderConfig config = new(bytes) { DelayedNNLoad = true };
-            DocumentReader.Instance().InitializeReader(Platform.AppContext, config, this);
+            // DocReaderConfig config = new(bytes) { DelayedNNLoad = true };
+            // DocumentReader.Instance().InitializeReader(Platform.AppContext, config, this);
         }
-        public void OnInitCompleted(bool success, DocumentReaderException error)
-        {
-            DocReaderInitEvent readerInitEvent = new() { IsSuccess = success };
-            if (!success)
-            {
-                Console.WriteLine("Init failed:" + error);
-                ScenariosObtained(this, readerInitEvent);
-                return;
-            }
-            IList<Scenario> data = [];
-            foreach (DocumentReaderScenario scenario in DocumentReader.Instance().AvailableScenarios)
-            {
-                var scenarion = new Scenario() { Name = scenario.Name, Description = scenario.Description };
-                data.Add(scenarion);
-            }
-            readerInitEvent.Scenarios = data;
-            readerInitEvent.IsRfidAvailable = DocumentReader.Instance().IsRFIDAvailableForUse;
-            ScenariosObtained(this, readerInitEvent);
-        }
+        // public void OnInitCompleted(bool success, DocumentReaderException error)
+        // {
+        //     DocReaderInitEvent readerInitEvent = new() { IsSuccess = success };
+        //     if (!success)
+        //     {
+        //         Console.WriteLine("Init failed:" + error);
+        //         ScenariosObtained(this, readerInitEvent);
+        //         return;
+        //     }
+        //     IList<Scenario> data = [];
+        //     foreach (DocumentReaderScenario scenario in DocumentReader.Instance().AvailableScenarios)
+        //     {
+        //         var scenarion = new Scenario() { Name = scenario.Name, Description = scenario.Description };
+        //         data.Add(scenarion);
+        //     }
+        //     readerInitEvent.Scenarios = data;
+        //     readerInitEvent.IsRfidAvailable = DocumentReader.Instance().IsRFIDAvailableForUse;
+        //     ScenariosObtained(this, readerInitEvent);
+        // }
 
         // BTDeviceSample
         static readonly int REQUEST_ENABLE_LOCATION = 196;
         static readonly int REQUEST_ENABLE_BT = 197;
         static bool isBleServiceConnected = false;
-        static BLEWrapper bleManager = null;
+        // static BLEWrapper bleManager = null;
         public void CheckPermissionsAndConnect(string btDeviceName)
         {
-            if (!IsBlePermissionGranted())
-            {
-                Console.WriteLine("ble permissions denied");
-                return;
-            }
-            if (isBleServiceConnected) return;
-            Console.WriteLine("Loading...");
-            DocumentReader.Instance().Functionality().Edit()
-                .SetUseAuthenticator(true)
-                .SetBtDeviceName(btDeviceName)
-                .Apply();
-            StartBluetoothService(Platform.CurrentActivity);
+            // if (!IsBlePermissionGranted())
+            // {
+            //     Console.WriteLine("ble permissions denied");
+            //     return;
+            // }
+            // if (isBleServiceConnected) return;
+            // Console.WriteLine("Loading...");
+            // DocumentReader.Instance().Functionality().Edit()
+            //     .SetUseAuthenticator(true)
+            //     .SetBtDeviceName(btDeviceName)
+            //     .Apply();
+            // StartBluetoothService(Platform.CurrentActivity);
         }
         public static bool IsBlePermissionGranted()
         {
-            if (!BluetoothSettingsHelper.IsLocationServiceEnabled(Platform.CurrentActivity))
-            {
-                RequestEnableLocationService(Platform.CurrentActivity);
-                return false;
-            }
-            string[] deniedPermissions = DeniedBluetoothPermissions(Platform.CurrentActivity);
-            if (deniedPermissions.Length > 0)
-            {
-                ActivityCompat.RequestPermissions(Platform.CurrentActivity, deniedPermissions, BluetoothPermissionHelper.BleAccessPermission);
-                return false;
-            }
-            if (!BluetoothSettingsHelper.IsBluetoothEnabled(Platform.CurrentActivity))
-            {
-                RequestEnableBle(Platform.CurrentActivity);
-                return false;
-            }
+            // if (!BluetoothSettingsHelper.IsLocationServiceEnabled(Platform.CurrentActivity))
+            // {
+            //     RequestEnableLocationService(Platform.CurrentActivity);
+            //     return false;
+            // }
+            // string[] deniedPermissions = DeniedBluetoothPermissions(Platform.CurrentActivity);
+            // if (deniedPermissions.Length > 0)
+            // {
+            //     ActivityCompat.RequestPermissions(Platform.CurrentActivity, deniedPermissions, BluetoothPermissionHelper.BleAccessPermission);
+            //     return false;
+            // }
+            // if (!BluetoothSettingsHelper.IsBluetoothEnabled(Platform.CurrentActivity))
+            // {
+            //     RequestEnableBle(Platform.CurrentActivity);
+            //     return false;
+            // }
             return true;
         }
         public static string[] DeniedBluetoothPermissions(Activity activity)
@@ -128,37 +128,37 @@ namespace DocumentReaderSample.Platforms.Android
         }
         public void StartBluetoothService(Activity activity)
         {
-            Intent bleIntent = new(activity, Java.Lang.Class.FromType(typeof(RegulaBleService)));
-            activity.StartService(bleIntent);
-            activity.BindService(bleIntent, this, 0);
+            // Intent bleIntent = new(activity, Java.Lang.Class.FromType(typeof(RegulaBleService)));
+            // activity.StartService(bleIntent);
+            // activity.BindService(bleIntent, this, 0);
         }
-        void IServiceConnection.OnServiceConnected(ComponentName name, IBinder service)
-        {
-            bleManager = (service as RegulaBleService.LocalBinder).Service.BleManager;
-            bool isBleManagerConnected = bleManager.IsConnected == true;
-            if (!isBleManagerConnected) bleManager.AddCallback(this);
-            isBleServiceConnected = true;
-            Console.WriteLine("BleService connected, connecting to device...");
-            Console.WriteLine("If nothing happens, then it failed to connect");
-        }
-        void IServiceConnection.OnServiceDisconnected(ComponentName name)
-        {
-            isBleServiceConnected = false;
-            Console.WriteLine("bleService disconnected");
-        }
-        public override void OnDeviceReady()
-        {
-            bleManager.RemoveCallback(this);
-            Console.WriteLine("device connected, initializing");
-            if (bleManager == null)
-            {
-                Console.WriteLine("bleManager is null");
-                return;
-            }
-            if (!DocumentReader.Instance().IsReady)
-                DocumentReader.Instance().InitializeReader(Platform.CurrentActivity, new BleDeviceConfig(bleManager), this);
-            else
-                Console.WriteLine("already initialized");
-        }
+        // void IServiceConnection.OnServiceConnected(ComponentName name, IBinder service)
+        // {
+        //     bleManager = (service as RegulaBleService.LocalBinder).Service.BleManager;
+        //     bool isBleManagerConnected = bleManager.IsConnected == true;
+        //     if (!isBleManagerConnected) bleManager.AddCallback(this);
+        //     isBleServiceConnected = true;
+        //     Console.WriteLine("BleService connected, connecting to device...");
+        //     Console.WriteLine("If nothing happens, then it failed to connect");
+        // }
+        // void IServiceConnection.OnServiceDisconnected(ComponentName name)
+        // {
+        //     isBleServiceConnected = false;
+        //     Console.WriteLine("bleService disconnected");
+        // }
+        // public override void OnDeviceReady()
+        // {
+        //     bleManager.RemoveCallback(this);
+        //     Console.WriteLine("device connected, initializing");
+        //     if (bleManager == null)
+        //     {
+        //         Console.WriteLine("bleManager is null");
+        //         return;
+        //     }
+        //     if (!DocumentReader.Instance().IsReady)
+        //         DocumentReader.Instance().InitializeReader(Platform.CurrentActivity, new BleDeviceConfig(bleManager), this);
+        //     else
+        //         Console.WriteLine("already initialized");
+        // }
     }
 }
