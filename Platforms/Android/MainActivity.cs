fa03b6f6c746f8e79481bb6859244fd20a7c08f1
Jenkins <jenkins@regula.by>
2024-09-30 14:17:34 +0300

    auto update

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.Runtime;
 
 namespace DocumentReaderSample;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
     internal static MainActivity Instance { get; private set; }
