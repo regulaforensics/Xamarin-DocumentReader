d2a6991772c439541ab2f55ec4d51c1106679a1c
Jenkins <jenkins@regula.by>
2025-04-02 17:44:13 +0300

    7.6.65-nightly

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,7 +5,9 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-		MainPage = new AppShell();
+	}
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		return new Window(new AppShell());
 	}
 }
-
