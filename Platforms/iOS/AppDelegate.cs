d087c8e0935c4e5cbd903ff2acf36355ebcbd3de
Jenkins <jenkins@regula.by>
2025-11-13 07:23:06 +0300

    8.4.1244-rc

--- Platforms/iOS/AppDelegate.cs
+++ Platforms/iOS/AppDelegate.cs
@@ -8,12 +8,6 @@ public class AppDelegate : MauiUIApplicationDelegate
 {
     protected override MauiApp CreateMauiApp()
     {
-        // WARNING: necessary!
-        // These 3 lines prevent MAUI from shrinking assemblies
-        new DocReaderCore.iOS.DocumentReader();
-        new RegulaCommon.iOS.RGLCCamera();
-        new BTDevice.iOS.RGLBTManager();
-
         DependencyService.Register<IDocReaderInit, DocReaderInit>();
         DependencyService.Register<IDocReaderScanner, DocReaderScanner>();
         DependencyService.Register<IPhotoPickerService, PhotoPickerService>();
